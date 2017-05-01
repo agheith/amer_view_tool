35772b805fff73224a176407b39ff17ae9ff0de8
agheith <amerghth@gmail.com>
2017-05-01 00:53:27 -0700

    initial commit

---
+++ lib/amer_view_tool.rb
@@ -0,0 +1,6 @@
+require "amer_view_tool/version"
+require "amer_view_tool/renderer"
+
+module AmerViewTool
+  # Your code goes here...
+end
---
+++ lib/amer_view_tool/version.rb
@@ -0,0 +1,3 @@
+module AmerViewTool
+  VERSION = "0.1.0"
+end
